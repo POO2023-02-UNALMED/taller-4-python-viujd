ed12aa708e226949889c85f6e3bdacb841ca9936
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-09-02 21:30:16 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,7 +1,7 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = "grado 12"
+    grado = "Grado 12"
 
     def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
         self._grupo = grupo
